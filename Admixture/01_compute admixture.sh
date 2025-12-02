06b6256785bab1238c3e682b3cd37ca4c9024ef3
SuperSu <119787166+superman96110@users.noreply.github.com>
2025-12-02 10:18:20 +0800

    Update 01_compute admixture.sh

--- Admixture/01_compute admixture.sh
+++ Admixture/01_compute admixture.sh
@@ -1,4 +1,9 @@
 #计算admixture，需要pruned的bfile文件
+#不仅需要bed文件，bim和fam文件都需要在当前文件夹下
+
+chmod +x run_admixutur.sh
+./run_admixutur.sh
+
 #!/bin/bash
 for k in {2..14}; do
     /home/jianglin/software/admixture_linux-1.3.0/admixture --cv sheep_snpname_filter_gtex_WGS_RNA_pruned_remove.bed $k | tee log${k}.out
